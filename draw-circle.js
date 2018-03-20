da5118868deba5af33751cb4c8bea59a5c979626
terence880 <36757988+terence880@users.noreply.github.com>
2018-03-20 15:14:59 +0800

    Add files via upload
    debugged

--- draw-circle.js
+++ draw-circle.js
@@ -7,32 +7,72 @@ class DrawingCircle extends PaintFunction{
     }
     
     onMouseDown(coord,event){
+        this.contextDraft.strokeStyle = "#f44";
+        this.contextDraft.lineWidth = 5;
         this.origX = coord[0];
         this.origY = coord[1];
     }
+
     onDragging(coord,event){
-        contextDraft.fillStyle = "#000000"
-        //this.contextDraft.strokeStyle = "#df4b26";
-        contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-        contextDraft.beginPath();
-        contextDraft.arc(this.origX, this.origY, (Math.abs(this.origX - coord[0])),0,360 )
-        contextDraft.closePath();
-        contextDraft.stroke();
+        this.contextDraft.strokeStyle = "#f44";
+        this.contextDraft.lineWidth = 5;
+        this.contextDraft.clearRect
+        (0,0,canvasDraft.width,canvasDraft.height);
+        this.contextDraft.beginPath();
+        this.contextDraft.arc
+        (this.origX, this.origY, (Math.abs(this.origX - coord[0])),0,360 );
+        this.contextDraft.stroke();
     }
 
-    onMouseMove(){
-        
-    }
+    onMouseMove(){ }
     onMouseUp(coord){
-
-        this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+        this.contextReal.strokeStyle = "#f44";
+        this.contextReal.lineWidth = 5;
+        this.contextDraft.clearRect
+        (0,0,canvasDraft.width,canvasDraft.height);
         this.contextReal.beginPath();
-        this.contextReal.arc(this.origX, this.origY, (Math.abs(this.origX - coord[0])),0,360 )
-        this.contextReal.closePath();
+        this.contextReal.arc
+        (this.origX, this.origY, (Math.abs(this.origX - coord[0])),0,360 );
         this.contextReal.stroke();
     }
-    onMouseLeave(){
-        
-    }
-    onMouseEnter(){}
+    onMouseLeave(){ }
+    onMouseEnter(){ }
 }
+
+// class DrawingCircle extends PaintFunction{
+  
+//     constructor(contextReal){
+//         super();
+//         this.contextReal = contextReal;
+//         this.contextDraft = contextDraft;            
+//     }
+    
+//     onMouseDown(coord,event){
+//         this.contextDraft.strokeStyle = "#f44";
+//         this.origX = coord[0];
+//         this.origY = coord[1];
+//     }
+
+//     onDragging(coord,event){
+//         this.contextDraft.strokeStyle = "#f44";
+//         this.contextDraft.clearRect
+//         (0,0,canvasDraft.width,canvasDraft.height);
+//         this.contextDraft.beginPath();
+//         this.contextDraft.arc
+//         (this.origX, this.origY, (this.origX - coord[0]),0,360 );
+//         this.contextDraft.stroke();
+//     }
+
+//     onMouseMove(){ }
+//     onMouseUp(coord){
+//         this.contextReal.strokeStyle = "#f44";
+//         this.contextDraft.clearRect
+//         (0,0,canvasDraft.width,canvasDraft.height);
+//         this.contextReal.beginPath();
+//         this.contextReal.arc
+//         (this.origX, this.origY, (this.origX - coord[0]),0,360 );
+//         this.contextReal.stroke();
+//     }
+//     onMouseLeave(){ }
+//     onMouseEnter(){ }
+// }
