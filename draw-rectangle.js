fab459b441ba3ccbc8b6af6d22c018d115090bd3
terence880 <36757988+terence880@users.noreply.github.com>
2018-03-23 09:24:47 +0800

    latest

--- draw-rectangle.js
+++ draw-rectangle.js
@@ -1,4 +1,5 @@
 class DrawingRectangle extends PaintFunction{
+
     constructor(contextReal,contextDraft){
         super();
         this.contextReal = contextReal;
@@ -6,20 +7,37 @@ class DrawingRectangle extends PaintFunction{
     }
     
     onMouseDown(coord,event){
-        this.contextReal.fillStyle = "#f44";
+        this.contextDraft.strokeStyle = rgbaColorFill;
+        this.contextDraft.lineWidth = 5;
+        this.contextDraft.fillStyle = rgbaColor;
         this.origX = coord[0];
         this.origY = coord[1];
+        this.contextDraft.stroke();
     }
     onDragging(coord,event){
-        this.contextDraft.fillStyle = "#f44";
+        this.contextDraft.strokeStyle = rgbaColorFill;
+        this.contextDraft.lineWidth = 5;
+        this.contextDraft.fillStyle = rgbaColor;
         this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+        this.contextDraft.beginPath();
         this.contextDraft.fillRect(this.origX,this.origY,coord[0]- this.origX,coord[1] - this.origY)
+        this.contextDraft.strokeRect(this.origX,this.origY,coord[0]- this.origX,coord[1] - this.origY)
+        this.contextDraft.stroke();
+        this.contextDraft.fill();
     }
 
     onMouseMove(){}
+
     onMouseUp(coord){
+        this.contextReal.strokeStyle = rgbaColorFill;
+        this.contextReal.lineWidth = 5;
+        this.contextReal.fillStyle = rgbaColor;
         this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+        this.contextReal.beginPath();
         this.contextReal.fillRect(this.origX,this.origY,coord[0]- this.origX,coord[1] - this.origY)
+        this.contextDraft.strokeRect(this.origX,this.origY,coord[0]- this.origX,coord[1] - this.origY)
+        this.contextReal.stroke();
+        this.contextReal.fill();
     }
     onMouseLeave(){}
     onMouseEnter(){}
